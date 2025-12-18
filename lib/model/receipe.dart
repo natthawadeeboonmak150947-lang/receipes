cf097b36cbe59517c97e9ab92fc676f9b47bb0c6
Natthawadee <natthawadeeboonmak150947@gmail.com>
2025-12-18 15:15:21 +0700

    Recipe Ingredient

--- lib/model/receipe.dart
+++ lib/model/receipe.dart
@@ -15,7 +15,7 @@ static List<Recipe> samples =
    'A spicy Thai salad made with grilled beef, fresh herbs, and chili-lime dressing.The bold, smoky flavor of the beef pairs perfectly with the tangy and spicy sauce,making it a vibrant and refreshing dish.'),
    Recipe('assets/images/2.webp', 'Thai pumpkin curry', 
    'A comforting Thai curry made with tender pumpkin and creamy coconut milk.The natural sweetness of the pumpkin balances beautifully with mild spices,resulting in a smooth and flavorful vegetarian-friendly dish.'),
- 
+  ];
 Recipe(this.imageUrl , this.imgLabel);
 
 static List<Recipe> samples = 
